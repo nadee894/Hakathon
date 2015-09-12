9cd011987c36c183942baa89cf3ce48ace16f2af
randyhesh <heshani7.herath@gmail.com>
2015-09-12 13:20:04 +0530

    menu

--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -18,8 +18,49 @@
         </ul>
     </li>
 
+    <li class="sub-menu">
+        <a href="javascript:;" id="user_menu">
+            <i class="fa fa-users"></i>
+            <span>Animal Management</span>
+        </a> 
+        <ul class="sub">
+            <li><a  href="<?php echo site_url(); ?>/users/manage_admins" onclick="">Manage Administrators</a></li>
+            <li><a  href="<?php echo site_url(); ?>/reg_users/manage_registered_users">Manage Registered Users</a></li>
+        </ul>
+    </li>
       
-
-
+    <li class="sub-menu">
+        <a href="javascript:;" id="user_menu">
+            <i class="fa fa-users"></i>
+            <span>Treatment Management</span>
+        </a> 
+        <ul class="sub">
+            <li><a  href="<?php echo site_url(); ?>/users/manage_admins" onclick="">Manage Administrators</a></li>
+            <li><a  href="<?php echo site_url(); ?>/reg_users/manage_registered_users">Manage Registered Users</a></li>
+        </ul>
+    </li>
+    
+    <li class="sub-menu">
+        <a href="javascript:;" id="user_menu">
+            <i class="fa fa-users"></i>
+            <span>Daily Log</span>
+        </a> 
+        <ul class="sub">
+            <li><a  href="<?php echo site_url(); ?>/users/manage_admins" onclick="">Manage Administrators</a></li>
+            <li><a  href="<?php echo site_url(); ?>/reg_users/manage_registered_users">Manage Registered Users</a></li>
+        </ul>
+    </li>
+    
+    <li class="sub-menu">
+        <a href="javascript:;" id="user_menu">
+            <i class="fa fa-users"></i>
+            <span>Reports</span>
+        </a> 
+        <ul class="sub">
+            <li><a  href="<?php echo site_url(); ?>/users/manage_admins" onclick="">Manage Administrators</a></li>
+            <li><a  href="<?php echo site_url(); ?>/reg_users/manage_registered_users">Manage Registered Users</a></li>
+        </ul>
+    </li>
+    
 </ul>
 
