bd927a1121b1fe14279db54df7303e69b1cac6c0
randyhesh <heshani7.herath@gmail.com>
2015-09-12 13:23:17 +0530

    db

--- zoo.sql
+++ zoo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.12
+-- version 4.2.11
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 12, 2015 at 09:29 AM
--- Server version: 5.6.25
--- PHP Version: 5.5.27
+-- Generation Time: Sep 12, 2015 at 09:52 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.5.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `zoo`
@@ -27,7 +27,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `animal` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   `place_id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL
@@ -40,8 +40,27 @@ CREATE TABLE IF NOT EXISTS `animal` (
 --
 
 CREATE TABLE IF NOT EXISTS `category` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `log`
+--
+
+CREATE TABLE IF NOT EXISTS `log` (
+`id` int(11) NOT NULL,
+  `animal_id` int(11) NOT NULL,
+  `bath_time` datetime DEFAULT NULL,
+  `breakfirst` datetime DEFAULT NULL,
+  `lunch` datetime DEFAULT NULL,
+  `diner` datetime DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `added_by` int(11) NOT NULL,
+  `is_deleted` enum('1','0') NOT NULL,
+  `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -51,7 +70,7 @@ CREATE TABLE IF NOT EXISTS `category` (
 --
 
 CREATE TABLE IF NOT EXISTS `places` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `block` varchar(100) NOT NULL,
   `cage` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -63,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `places` (
 --
 
 CREATE TABLE IF NOT EXISTS `treatment` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `animal_id` int(11) NOT NULL,
   `veteren_id` int(11) NOT NULL,
   `treatment` varchar(100) NOT NULL,
@@ -78,13 +97,14 @@ CREATE TABLE IF NOT EXISTS `treatment` (
 --
 
 CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `title` varchar(100) DEFAULT NULL,
   `name` varchar(300) NOT NULL,
   `user_name` varchar(300) NOT NULL,
   `user_type` int(11) NOT NULL,
   `email` varchar(200) NOT NULL,
   `address` varchar(500) DEFAULT NULL,
+  `profile_pic` varchar(500) DEFAULT NULL,
   `contact_no_1` varchar(15) DEFAULT NULL,
   `is_deleted` enum('0','1') NOT NULL DEFAULT '0',
   `added_by` int(11) DEFAULT NULL,
@@ -98,7 +118,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 CREATE TABLE IF NOT EXISTS `user_type` (
-  `id` int(11) NOT NULL,
+`id` int(11) NOT NULL,
   `type` varchar(300) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
@@ -121,37 +141,43 @@ INSERT INTO `user_type` (`id`, `type`) VALUES
 -- Indexes for table `animal`
 --
 ALTER TABLE `animal`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `category`
 --
 ALTER TABLE `category`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `log`
+--
+ALTER TABLE `log`
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `places`
 --
 ALTER TABLE `places`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `treatment`
 --
 ALTER TABLE `treatment`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `user_type`
 --
 ALTER TABLE `user_type`
-  ADD PRIMARY KEY (`id`);
+ ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -161,32 +187,37 @@ ALTER TABLE `user_type`
 -- AUTO_INCREMENT for table `animal`
 --
 ALTER TABLE `animal`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `log`
+--
+ALTER TABLE `log`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `places`
 --
 ALTER TABLE `places`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `treatment`
 --
 ALTER TABLE `treatment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `user_type`
 --
 ALTER TABLE `user_type`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
